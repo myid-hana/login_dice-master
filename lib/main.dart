58cc829f440a4bf9702d92a020984a6815e28a2c
myid-hana <sosqkrgksml@gmail.com>
2021-05-31 16:45:23 +0900

    로그인 페이지 디자인

--- lib/main.dart
+++ lib/main.dart
@@ -26,18 +26,68 @@ class _LogInState extends State<LogIn> {
         title: Text('Log in'),
         backgroundColor: Colors.redAccent,
         centerTitle: true,
-        leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: (){}
-        ),
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
         actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.search),
-              onPressed: (){}
-          )
+          IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top: 50)),
+            Center(
+              child: Image(
+                image: AssetImage('image/chef.gif'),
+                width: 170,
+                height: 190,
+              ),
+            ),
+            Form(
+                child: Theme(
+              data: ThemeData(
+                  primaryColor: Colors.teal, //input underline color
+                  inputDecorationTheme: InputDecorationTheme(
+                      //input label style
+                      labelStyle: TextStyle(color: Colors.teal, fontSize: 15))),
+              child: Container(
+                padding: EdgeInsets.all(
+                    40), //container 는 하나의 child 밖에 가질 수 없으므로 column 을 불러온다.
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Enter "dice"' //input label
+                          ),
+                      keyboardType: TextInputType.emailAddress, //키보드 타입
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Enter Password' //input label
+                          ),
+                      keyboardType: TextInputType.text, //키보드 타입
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ButtonTheme(
+                      minWidth: 100,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.green),),
+                        child: Text(
+                          'login',
+                        ),
+                        onPressed: () {},
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ))
+          ],
+        ),
       ),
     );
   }
 }
-
