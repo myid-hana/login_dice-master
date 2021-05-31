a8448aa91208aeb5b105b9c36b7710293800b6ab
myid-hana <sosqkrgksml@gmail.com>
2021-05-31 17:54:00 +0900

    if 조건에 따른 페이지 이동, 스낵바 구현, Dice 페이지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Dice.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +20,10 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+  TextEditingController controller =
+      TextEditingController(); //Textfield 에 입력되는 값 가져오기 위한 컨트롤러
+  TextEditingController controller2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,63 +36,122 @@ class _LogInState extends State<LogIn> {
           IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.only(top: 50)),
-            Center(
-              child: Image(
-                image: AssetImage('image/chef.gif'),
-                width: 170,
-                height: 190,
-              ),
-            ),
-            Form(
-                child: Theme(
-              data: ThemeData(
-                  primaryColor: Colors.teal, //input underline color
-                  inputDecorationTheme: InputDecorationTheme(
-                      //input label style
-                      labelStyle: TextStyle(color: Colors.teal, fontSize: 15))),
-              child: Container(
-                padding: EdgeInsets.all(
-                    40), //container 는 하나의 child 밖에 가질 수 없으므로 column 을 불러온다.
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: 'Enter "dice"' //input label
-                          ),
-                      keyboardType: TextInputType.emailAddress, //키보드 타입
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: 'Enter Password' //input label
-                          ),
-                      keyboardType: TextInputType.text, //키보드 타입
-                      obscureText: true,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    ButtonTheme(
-                      minWidth: 100,
-                      height: 50,
-                      child: ElevatedButton(
-                        style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.green),),
-                        child: Text(
-                          'login',
-                        ),
-                        onPressed: () {},
-                      ),
-                    )
-                  ],
+      body: Builder(builder: (context) {
+        return SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 50)),
+              Center(
+                child: Image(
+                  image: AssetImage('image/chef.gif'),
+                  width: 170,
+                  height: 190,
                 ),
               ),
-            ))
-          ],
-        ),
-      ),
+              Form(
+                  child: Theme(
+                data: ThemeData(
+                    primaryColor: Colors.teal, //input underline color
+                    inputDecorationTheme: InputDecorationTheme(
+                        //input label style
+                        labelStyle:
+                            TextStyle(color: Colors.teal, fontSize: 15))),
+                child: Container(
+                  padding: EdgeInsets.all(
+                      40), //container 는 하나의 child 밖에 가질 수 없으므로 column 을 불러온다.
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: controller, //컨트롤러 설정해주기.
+                        decoration: InputDecoration(
+                            labelText: 'Enter "dice"' //input label
+                            ),
+                        keyboardType: TextInputType.emailAddress, //키보드 타입
+                      ),
+                      TextField(
+                        controller: controller2,
+                        decoration: InputDecoration(
+                            labelText: 'Enter Password' //input label
+                            ),
+                        keyboardType: TextInputType.text, //키보드 타입
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ButtonTheme(
+                        minWidth: 100,
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.green),
+                          ),
+                          child: Text(
+                            'login',
+                          ),
+                          onPressed: () {
+                            //조건에 따라 페이지 이동
+                            if (controller.text == 'dice' &&
+                                controller2.text == '1234') {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (BuildContext context) =>
+                                          Dice()));
+                            } else if (controller.text != 'dice') {
+                              showSnackBar2(context);
+                            } else if (controller2.text != '1234') {
+                              showSnackBar3(context);
+                            }
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ))
+            ],
+          ),
+        );
+      }),
     );
   }
+}
+
+void showSnackBar(BuildContext context) {
+  Scaffold.of(context).showSnackBar(
+    SnackBar(
+        content: Text(
+          '로그인 정보를 다시 확인하세요.',
+          textAlign: TextAlign.center,
+        ),
+        duration: Duration(seconds: 2),
+        backgroundColor: Colors.pink),
+  );
+}
+
+void showSnackBar2(BuildContext context) {
+  Scaffold.of(context).showSnackBar(
+    SnackBar(
+        content: Text(
+          '아이디가 일치하지 않습니다.',
+          textAlign: TextAlign.center,
+        ),
+        duration: Duration(seconds: 2),
+        backgroundColor: Colors.pink),
+  );
+}
+
+void showSnackBar3(BuildContext context) {
+  // ignore: deprecated_member_use
+  Scaffold.of(context).showSnackBar(
+    SnackBar(
+        content: Text(
+          '비밀번호가 일치하지 않습니다.',
+          textAlign: TextAlign.center,
+        ),
+        duration: Duration(seconds: 2),
+        backgroundColor: Colors.pink),
+  );
 }
